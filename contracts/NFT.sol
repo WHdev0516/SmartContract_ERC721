81b20d64584bb14f92eabbf08b231c05a70585b7
WHdev0516 <appc31058@gmail.com>
2022-01-27 19:28:18 +0300

    adding metadata

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,8 +7,13 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract NFT is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private currentTokenId;
+
+    // @dev bse token URI used as a prefix by tokenURI()
+    string public baseTokenURI;
     
-    constructor() ERC721("NFTTutorial", "NFT") {}
+    constructor() ERC721("CiMPLE", "NFT") {
+        baseTokenURI = "";
+    }
     
     function mintTo(address recipient)
         public
@@ -19,4 +24,15 @@ contract NFT is ERC721 {
         _safeMint(recipient, newItemId);
         return newItemId;
     }
+
+    // @dev Returns an URI for a given token ID
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenURI;
+    }
+
+    // @dev Sets the base token URI prefix
+    function setBaseTokenURI(string memory _baseTokenURI) public {
+        baseTokenURI = _baseTokenURI;
+    }
+
 }
