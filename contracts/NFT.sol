5d322deee2fc5240412d8073b8fba330bbf3aeac
WHdev0516 <appc31058@gmail.com>
2022-01-28 19:58:52 +0300

    Setting price and Withdrawing Funds

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,12 +3,16 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/PullPayment.sol";
 
-contract NFT is ERC721 {
+contract NFT is ERC721, PullPayment {
     using Counters for Counters.Counter;
 
     // Constants. Define total supply.
     uint256 public constant TOTAL_SUPPLY =10_000;
+
+    // Constants. Define MINT price.
+    uint256 public constant MINT_PRICE = 0.008 ether;
 
     Counters.Counter private currentTokenId;
 
@@ -19,13 +23,12 @@ contract NFT is ERC721 {
         baseTokenURI = "";
     }
     
-    function mintTo(address recipient)
-        public
-        returns (uint256)
+    function mintTo(address recipient) public payable returns (uint256)
     {
         uint256 tokenId =  currentTokenId.current();
         require(tokenId < TOTAL_SUPPLY, "Max supply reached");
-        
+        require(msg.value == MINT_PRICE, "Transaction value did not equal the mint price");
+
         currentTokenId.increment();
         uint256 newItemId = currentTokenId.current();
         _safeMint(recipient, newItemId);
