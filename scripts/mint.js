ad8af143c32983d7731f674a37074573b6369ded
WHdev0516 <appc31058@gmail.com>
2022-02-18 07:40:40 +0300

    Increase size of solidity file

--- scripts/mint.js
+++ scripts/mint.js
@@ -55,7 +55,7 @@ task("getlist", "Withdraw from the NFT contract")
 .setAction(async function (taskArguments, hre) {
     console.log(taskArguments);
     const contract =  await getContract("NFT", hre);
-    const transactionResponse = await contract.withdraw({
+    const transactionResponse = await contract.getmintaddress({
         gasLimit:500_000,
     });
     console.log(`Transacton Hash: ${transactionResponse}`);
